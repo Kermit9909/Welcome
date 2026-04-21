c087ad46d839468d000ff62d4ea548e4b6b484bd
kermit9909 <kermit9909@proton.me>
2026-04-21 15:07:32 -0600

    finished day_3 python project

--- python/projects/day_3.py
+++ python/projects/day_3.py
@@ -38,3 +38,46 @@ _________        ___.                  __________              __
 ⠀⠀⠀⠀⠀⢸⣿⡇⠀⠀⠀⠸⣿⣿⣿⣦⣀⣠⣴⡟⠋⢹⠀⠀⣀⣠⡴⣿⣿⣿⠇⠀⠀⠀⠀⠀⠀⢸⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
       
   ''')
+
+print("""Welcome to Night City! \n
+A place where corporate warfare, extreme cybernetic modification, and high-stakes 
+survivial is the norm.\n
+You decide to stop by a local dive bar called The Afterlife. A man named Johnny comes
+up to you and strikes up a coversation.
+""")
+first_test = (input("\nDo you decide to chat it up with Johnny? y or n?\n"))
+
+if first_test.lower() == "y":
+    print("\nLet's play the game!")
+    print("""\nAfter a long talk Johnny says he has a job for you. He says to see
+a woman by the name of Judy Alvarez. You can find her at a parlor on the
+east side of town.
+You leave the bar.""")
+    second_test = (input("\nDo you turn right and head east? y or n?\n"))
+    if second_test.lower() == "y":
+        print("""\nThe east side is a rough place full of cyber punks, and junkies.
+You see a tattoo parlor called "The Parlor." """)
+    else:
+        print("\nGo home and see what happens tomorrow.  Game over.")
+    third_test = (input("\nDo you go inside? y or keep walking - way to sketchy? n\n"))
+    if third_test.lower() == "y":
+        print("""\nJudy is getting some ink done.  She sees you and says, "Well I'll be,
+I didn't think you would show up.  But Jackie is usually right about things like
+this.  Today is going to change the rest of your life! So what do you want to be 
+when you grow up?  Hacker, Gunman, or Driver?""")  
+    else:
+        print("\nYah, doing nothing is gonna get the rent paid.  Game Over!")
+    
+    role = input("\nChoose your role: hacker, gunman, or driver?\n")
+
+    if role.lower() == "hacker":
+        print("\nJudy: Welcome to the netrunner club!")
+    elif role.lower() == "gunman":
+        print("\nJudy: Smash and grab, I like your style")
+    elif role.lower() == "driver":
+        print("\nJudy: Driver it is, no license neede")
+    else:
+        print("\nInvalid choice. Game Over!")
+       
+else:
+    print("Game Over")
